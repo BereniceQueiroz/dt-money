eca044011ed8fa0e8850fdd5136d68f926c56143
BereniceQueiroz <baqtecnologia@gmail.com>
2022-01-23 16:29:48 -0300

    chore: add axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import * as S from './styles';
 
 function TransactionTable() {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, []);
+
   return (
     <S.Wrapper>
       <S.Table>
