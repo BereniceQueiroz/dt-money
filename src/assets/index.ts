2182bea9db00cf500bbd806e4ed4fb62c4fd56ad
BereniceQueiroz <baqtecnologia@gmail.com>
2022-01-23 09:56:08 -0300

    feat: initial commit

---
+++ src/assets/index.ts
@@ -0,0 +1,19 @@
+import icon from './png/favicon.png';
+
+import { ReactComponent as Logo } from './svg/logo.svg';
+import { ReactComponent as Close } from './svg/close.svg';
+import { ReactComponent as Income } from './svg/income.svg';
+import { ReactComponent as Outcome } from './svg/outcome.svg';
+import { ReactComponent as Total } from './svg/total.svg';
+
+export const icons = {
+  icon,
+};
+
+export const components = {
+  Close,
+  Income,
+  Outcome,
+  Total,
+  Logo,
+};
